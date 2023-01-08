62bfe11e12841951d9888a56119a52c6bb9794ed
thisisclint21 <thisisclint21@users.noreply.github.com>
2023-01-08 18:02:26 +0000

    Update package

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+export * from "./HypeProxy";
+export * from "./HypeProxy.Constants";
+export * from "./HypeProxy.Dtos";
+export * from "./HypeProxy.Entities";
+export * from "./HypeProxy.Entities.ApplicationUser";
+export * from "./HypeProxy.Entities.Price";
+export * from "./HypeProxy.Responses";
+export * from "./HypeProxy.Responses.ApiResponse";
+export * from "./HypeProxy.Responses.Insights";
+export * from "./HypeProxy.Responses.InsightsSummary";
