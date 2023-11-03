dc965200239dd3366a289f50fa97749386c74917
imclint21 <imclint21@users.noreply.github.com>
2023-11-03 10:27:02 +0000

    Update package

--- src/HypeProxy.Constants.ts
+++ src/HypeProxy.Constants.ts
@@ -2,10 +2,6 @@
 /* eslint-disable */
 /* tslint:disable */
 
-/** Transpiled from HypeProxy.Constants.ApiErrorCodes */
-export type ApiErrorCodes = {
-}
-
 /** Transpiled from HypeProxy.Constants.ApiErrorCodes.Authentication */
 export type Authentication = {
 }
