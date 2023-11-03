1c130e3110205cae836a4393104ddded77182e17
Clint Mourlevat <5221349+imclint21@users.noreply.github.com>
2023-11-03 11:25:07 +0100

    Add ApiErrorCodes

--- src/HypeProxy.ts
+++ src/HypeProxy.ts
@@ -4,3 +4,20 @@ export interface ApiResponseFor<T> extends ApiResponse
 {
 	data: T;
 }
+
+export const ApiErrorCodes = {
+    UnknownError: "UNKNOWN_ERROR",
+    PrivateApi: "PRIVATE_API",
+
+    Authentication: {
+        MissingToken: "MISSING_TOKEN",
+        ExpiredToken: "EXPIRED_TOKEN",
+        MalformedToken: "MALFORMED_TOKEN",
+    },
+
+    User: {
+        UserNotFound: "USER_NOT_FOUND",
+        UserNotAllowed: "USER_NOT_ALLOWED",
+        MissingUserId: "MISSING_USER_ID",
+    }
+}
