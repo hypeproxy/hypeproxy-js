523ed7b19cd0a3d76024d36a6563312e4210419a
clint21.eth <me@clint21.dev>
2023-11-12 11:05:39 +0100

    Use build folder

--- local-build.sh
+++ local-build.sh
@@ -1,8 +1,16 @@
-git pull
-rm -rf dist
-mv src/HypeProxy.ts ./HypeProxy.ts
-tapper --project ../hypeproxy-dotnet/src/HypeProxy/HypeProxy.csproj --output ./src/ --enum name
-mv ./HypeProxy.ts src/HypeProxy.ts
-find ./src -name "*.ts" ! -name "index.ts" | xargs basename -s .ts | sort | xargs -I '{}' bash -c 'echo "export * from \"./{}\";"' >> ./src/index.ts
+#!/bin/bash
+
+# Transpile C# entities to TS.
+tapper --project ../hypeproxy-dotnet/src/HypeProxy/HypeProxy.csproj --output ./build --enum name
+cp -R ./src/* ./build
+
+# Generate index.ts global export.
+rm -rf ./build/index.ts
+find ./build -name "*.ts" ! -name "index.ts" | xargs basename -s .ts | sort | xargs -I '{}' bash -c 'echo "export * from \"./{}\";"' >> ./build/index.ts
+
+# Build the package.
 pnpm install
 pnpm build
+
+# Cleanup
+rm -rf build
